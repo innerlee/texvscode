7f3f82e3c121927e647be2472aca00e3e4812510
innerlee <363664470@qq.com>
2017-11-17 23:15:52 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -15,11 +15,67 @@ const filelist = [
     'gulpfile.js'
 ]
 
+const execoptions = {
+    encoding: 'utf8',
+    timeout: 10000,
+    cwd: './sub_modules/texvscode',
+    env: null
+};
+
 function isSubmoduleReady() {
     return fs.existsSync('sub_modules/texvscode')
 }
 
-gulp.task('MoveToSubmodule', function (cb) {
+async function isProjectClean() {
+    try {
+        status = await new Promise(
+            function (resolve, reject) {
+                cp.exec('git status --porcelain --ignore-submodules', function (err, stdout, stderr) {
+                    resolve(stdout.trim());
+                    reject(stderr);
+                });
+            });
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+    if (status === '') {
+        return true
+    } else {
+        console.log('repo status:')
+        console.log(status)
+        return false
+    }
+}
+
+async function isSubmoduleClean() {
+    try {
+        status = await new Promise(
+            function (resolve, reject) {
+                cp.exec('git status --porcelain', execoptions, function (err, stdout, stderr) {
+                    resolve(stdout.trim());
+                    reject(stderr);
+                });
+            });
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+    if (status === '') {
+        return true
+    } else {
+        console.log('submodule status:')
+        console.log(status)
+        return false
+    }
+}
+
+gulp.task('MoveToSubmodule', async function (cb) {
+    if (!await isSubmoduleClean()) {
+        console.warn('submodule not clean, please first commit!')
+        return
+    }
+
     for (const f of filelist) {
         console.log(`copying ${f}`)
         cpFile(f, path.join('sub_modules/texvscode', f)).then(() => {
@@ -28,14 +84,22 @@ gulp.task('MoveToSubmodule', function (cb) {
     }
 })
 
+gulp.task('UpdateFromSubmodule', async function (cb) {
+    if (!await isProjectClean()) {
+        console.warn('project not clean, please first commit!')
+        return
+    }
+
+    for (const f of filelist) {
+        console.log(`copying ${f}`)
+        cpFile(path.join('sub_modules/texvscode', f), f).then(() => {
+            console.log(`done ${f}.`);
+        });
+    }
+})
+
 gulp.task('PullSubmodule', function (cb) {
-    const options = {
-        encoding: 'utf8',
-        timeout: 10000,
-        cwd: './sub_modules/texvscode',
-        env: null
-    };
-    cp.exec('git pull origin master', options, function (err, stdout, stderr) {
+    cp.exec('git pull origin master', execoptions, function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
